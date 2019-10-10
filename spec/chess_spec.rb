6c03ea4a33891d4d073a2939eb27796a979b71d1
Ian Kesler <ianmkesler@gmail.com>
2019-10-10 14:33:19 -0700

    Add Bishop tests

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,14 +1,15 @@
 require_relative "../lib/king"
+require_relative "../lib/bishop"
 describe King do
+    it 'places in the appropriate starting position' do
+        black = King.new('black')
+        expect(black.position).to eql([0,4])
+
+        white = King.new('white')
+        expect(white.position).to eql([7,4])
+    end
+
     describe "#.move" do
-        it 'places in the appropriate starting position' do
-            black = King.new('black')
-            expect(black.position).to eql([0,4])
-
-            white = King.new('white')
-            expect(white.position).to eql([7,4])
-        end
-
         it 'returns false for an invalid move' do
             king = King.new('black')
             expect(king.move([2,2])).to be false
@@ -25,6 +26,70 @@ describe King do
                 expect(king.position).to eql(move)
                 king.position = [3,5]
             end         
+        end
+    end
+end
+
+describe Bishop do
+    it 'places in the appropriate starting position' do
+        black1 = Bishop.new('black')
+        expect(black1.position).to eql([0,2])
+
+        black2 = Bishop.new('black')
+        expect(black2.position).to eql([0,5])
+
+        Bishop.reset_count
+
+        white1 = Bishop.new('white')
+        expect(white1.position).to eql([7,2])
+
+        white2 = Bishop.new('white')
+        expect(white2.position).to eql([7,5])
+
+        Bishop.reset_count
+    end
+
+    describe '#.construct_moves' do
+        it 'constructs all possible moves' do
+            bishop = Bishop.new('black')
+            possible_moves = [[1,3], [2,4], [3,5], [4,6] ,[5,7], [1,1], [2,0]]
+            expect(bishop.send(:construct_moves)).to eql(possible_moves)
+        end
+    end
+
+    describe '#.valid_move?' do
+        it 'returns true for a valid move' do
+            Bishop.reset_count
+            bishop = Bishop.new('black')
+            possible_moves = [[1,3], [2,4], [3,5], [4,6] ,[5,7], [1,1], [2,0]]
+            possible_moves.each do |move|
+                expect(bishop.send(:valid_move?, move)).to be true
+            end
+        end
+
+        it 'returns false for an invalid move' do
+            bishop = Bishop.new('black')
+            expect(bishop.send(:valid_move?, [0,0])).to be false
+        end
+    end
+
+    describe "#.move" do
+        it 'returns false for an invalid move' do
+            Bishop.reset_count
+            bishop = Bishop.new('black')
+            expect(bishop.move([2,2])).to be false
+            expect(bishop.position).to eql([0,2])
+        end
+
+        it 'changes the position and returns true for a valid move' do
+            Bishop.reset_count
+            bishop = Bishop.new('black')
+            possible_moves = [[1,3], [2,4], [3,5], [4,6] ,[5,7], [1,1], [2,0]]
+            possible_moves.each do |move|
+                expect(bishop.move(move)).to be true
+                expect(bishop.position).to eql(move)
+                bishop.position = [0,2]
+            end
         end
     end
 end
